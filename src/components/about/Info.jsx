176e3aa3248b8a49133f5a3feb7f6a746f2c0941
Paresh-Kumar14 <pareshkbehera14@gmail.com>
2023-11-18 18:40:07 +0530

    changes in About

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 const info = () => {
     return (
         <div className='about__info grid'>
-            <div className='about__box'>
-            <i class='bx bx-award about__icon'></i>
+         {/* <div className='about__box'> 
+            <i className='bx bx-award about__icon'></i>
             
                 <h3 className='about__title'>Experience</h3>
                 <span className='about__subtitle'>fresher</span>
-            </div>
+        </div>  */}
             <div className='about__box'>
-            <i class="bx bx-briefcase-alt about__icon"></i>
+            <i className="bx bx-briefcase-alt about__icon"></i>
                 <h3 className='about__title'>completed</h3>
                 <span className='about__subtitle'> 3projects</span>
             </div>
             <div className='about__box'>
-            <i class="bx bx-support about__icon"></i>
-                <h3 className='about__title'>support</h3>
-                <span className='about__subtitle'>online 24/7</span>
+            <i className="bx bx-support about__icon"></i>
+                <h3 className='about__title'>Contact Me</h3>
+                <span className='about__subtitle'></span>
             </div>
 
         </div>
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,13 +4,13 @@ const Social = () => {
   return (
     <div className="home__social">
       <a href="https://instagram.com/" className="home__social-icon" target="_blank" rel="noreferrer">
-        <i class="uil uil-instagram"></i>
+        <i className="uil uil-instagram"></i>
       </a>
-      <a href="https://linkdin.com/" className="home__social-icon" target="_blank" rel="noreferrer">
-      <i class="uil uil-linkedin"></i>
+      <a href="www.linkedin.com/in/paresh750450" className="home__social-icon" target="_blank" rel="noreferrer">
+      <i className="uil uil-linkedin"></i>
       </a>
-      <a href="https://github.com/" className="home__social-icon" target="_blank" rel="noreferrer">
-        <i class="uil uil-github-alt"></i>
+      <a href="https://github.com/Paresh-Kumar14/" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i className="uil uil-github-alt"></i>
       </a>
 
     </div>
