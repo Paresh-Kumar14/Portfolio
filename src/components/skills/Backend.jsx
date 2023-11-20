a126b72ff177d38cd7fcb9a08eb0cfc1467cfef3
Paresh-Kumar14 <pareshkbehera14@gmail.com>
2023-11-20 23:54:21 +0530

    css modify

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -30,7 +30,7 @@ const Backend = () => {
                         </div>
                     </div>
                     <div className="skills__data">
-                        <i class="bx bx-badge-check"></i>
+                        <i className="bx bx-badge-check"></i>
                         <div>
                             <h3 className="skills__name">Express Js</h3>
                             <span className="skills__level">Intermediate</span>
